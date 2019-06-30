3439fc869bf4fc366c5fb91a9bceedf87ebe096e
zhangdongming <154513394@qq.com>
2019-06-30 21:23:24 +0800

    添加test工程至git

---
+++ src/cn/itcast/HelloGit.java
@@ -0,0 +1,11 @@
+package cn.itcast;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+        System.out.println("1");
+    }
+}
